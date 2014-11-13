3f2e481d9a1bd32b6c87afa7c1439095578e98ee
David Jones <unixmonkey1@gmail.com>
2014-11-12 19:48:11 -0500

    Log user in when they sign up, too

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: t('user.flash.create.success')
     else
       flash.now[:alert] = t('user.flash.create.failure')
